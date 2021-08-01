188cbdfcd3eeee809c17280dbac3fadfae5042f5
David Barbour <dmbarbour@gmail.com>
2021-07-31 21:59:41 -0500

    preliminary support for transactional logging

--- src/Effects.fs
+++ src/Effects.fs
@@ -70,3 +70,81 @@ module Effects =
             member __.Abort () = ()
         }
 
+    /// As wrapFail except we may change the `fail` label.
+    let wrapFailTag lbl =
+        let k = Value.label lbl
+        let startsWithLbl v = Option.isSome (Value.tryMatchStem k v)
+        fun msgList -> 
+            if Seq.forall startsWithLbl (FTList.toSeq msgList) 
+                then msgList // flatten if empty list or all log messages are failures
+                else msgList |> Value.ofFTList |> Value.variant lbl |> FTList.singleton
+    
+    /// wrapFail is the default rewrite function for transactional logging support.
+    /// Primarily, we'll rewrite a list of aborted log messages to a single message
+    /// of form `fail:MsgList`. There is also logic to flatten a stack of failures. 
+    let wrapFail = wrapFailTag "fail"
+
+    /// Transactional Logging Support
+    /// 
+    /// Logging requires special attention in context of hierarchical
+    /// transactions and backtracking. We should keep the log messages
+    /// because they are precious for debugging. However, which log
+    /// messages are committed or aborted should also be clear to the
+    /// client.
+    ///
+    /// The default method to group aborted log messages under 
+    /// `fail:[MessageList]`. Users may override this behavior, 
+    /// e.g. for contexts where label 'fail' is used.
+    ///
+    /// At the top-level, a function can be provided to receive the 
+    /// logged messages when they become available.
+    type TXLogSupport =
+        val private LogOut : Value -> unit
+        val private WrapFail : FTList<Value> -> FTList<Value>
+        val mutable private TXStack : FTList<Value> list
+
+        /// Set the committed output destination. 
+        /// Wrap aborted messages with `fail:MessageList`.
+        new (out) = 
+            { LogOut = out; WrapFail = wrapFail; TXStack = [] }
+
+        /// Set the commited output destination and the rewrite
+        /// function for handling aborted aborted messages. 
+        new (out, rewriteF) = 
+            { LogOut = out; WrapFail = rewriteF; TXStack = [] }
+
+        member self.Log(msg : Value) : unit =
+            match self.TXStack with
+            | (tx0::txs) ->
+                self.TXStack <- (FTList.snoc tx0 msg)::txs
+            | [] -> 
+                self.LogOut msg // commit immediately
+
+        member self.PushTX () : unit = 
+            self.TXStack <- (FTList.empty) :: self.TXStack
+
+        member self.PopTX (bCommit : bool) : unit =
+            match self.TXStack with
+            | [] -> invalidOp "pop empty transaction stack"
+            | (tx0::tx0Rem) ->
+                let tx0' = if bCommit then tx0 else self.WrapFail tx0
+                match tx0Rem with
+                | (tx1::txs) -> // commit into lower transaction
+                    self.TXStack <- (FTList.append tx1 tx0')::txs 
+                | [] -> // commit to output
+                    self.TXStack <- List.empty
+                    for msg in FTList.toSeq tx0' do
+                        self.LogOut msg
+
+        interface IEffHandler with
+            member self.Eff v =
+                match v with
+                | Value.Variant "log" msg -> 
+                    self.Log(msg)
+                    Some Value.unit
+                | _ -> None
+        interface ITransactional with
+            member self.Try () = self.PushTX ()
+            member self.Commit () = self.PopTX true
+            member self.Abort () = self.PopTX false
+
